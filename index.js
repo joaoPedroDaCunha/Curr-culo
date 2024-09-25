ad12dd52a2d4e7f097b2096adfa6cc1984bcbd33
JoaoPedroDaCunha <joaopedrodacunha.santos@gmail.com>
2024-09-25 13:25:51 -0300

    falha

---
+++ index.js
@@ -0,0 +1,8 @@
+const withMt = require('@material-tailwind/html/utils.withMT');
+module.exports = withMt({
+    content:["./index.html"],
+    theme:{
+        extends:{},
+    },
+    plugins:[],
+})
