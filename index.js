a8acbe9ed31d17addc2be245b62c6d26d5a44861
JoaoPedroDaCunha <joaopedrodacunha.santos@gmail.com>
2024-09-26 08:47:49 -0300

    ajust model vanilla

--- index.js
+++ index.js
@@ -1,8 +1,25 @@
-const withMt = require('@material-tailwind/html/utils.withMT');
-module.exports = withMt({
-    content:["./index.html"],
-    theme:{
-        extends:{},
-    },
-    plugins:[],
+import {Component} from '@angular/core';
+import {bootstrapApplication} from '@angular/platform-browser';
+import {withMT} from "@material-tailwind/html/utils/withMT";
+import { selectors } from 'sizzle';
+
+@Component({
+    selector: 'app-root',
+   standalone: true,
+   template: `
+    Hello world!
+  `,
 })
+export class main{}
+
+bootstrapApplication(main);
+ 
+/** @type {import('tailwindcss').Config} */
+module.exports = withMT({
+    content: ["./index.html",
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  });
